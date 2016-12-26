7f3be9457e65b8a22b18ac6ac78c90948cb32095
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2016-12-26 17:37:20 -0200

    Alterações no menu

--- menu.php
+++ menu.php
@@ -1,10 +1,20 @@
+<?php
 
+  $pagina = $_GET['link'];
+  if ($pagina == 1){
+    $caminho = "class='nav-link active' href='index.php'";
+  }
+  else{
+    $caminho = "class='nav-link' href='index.php'";
+      }
+
+?>
               <h3 class="masthead-brand">ABVIP</h3>
               <nav class="nav nav-masthead">
-                <a class="nav-link active" href="index.php">Principal</a>
-                <a class="nav-link" href="#">Atendimentos</a>
-                <a class="nav-link" href="#">Quem somos</a>
-                <a class="nav-link" href="#">Fale conosco</a>
-                <a class="nav-link" href="login.php">Restrito</a>
+                <a <?=$caminho?>>Principal</a>
+                <a class="nav-link" href="link=2">Atendimentos</a>
+                <a class="nav-link" href="link=3">Quem somos</a>
+                <a class="nav-link" href="link=4">Fale conosco</a>
+                <a class="nav-link" href="link=5">Restrito</a>
               </nav>
 
